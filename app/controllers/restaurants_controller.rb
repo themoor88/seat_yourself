da473d612d25870b289b10230298a4f85120f2bd
Wajid Kagzi <wjkagzi@gmail.com>
2015-04-24 15:00:06 -0400

    Added Restaurant views and routes.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,49 @@
-class RestaurantsController < ActiveController
+class RestaurantsController < ApplicationController
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurants_path
+    else
+      render :new
+    end
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to @restaurant
+    else
+      render :edit
+    end
+
+  end
+
+  def destroy
+    @restaurant = Restaurant(params[:id])
+    @restaurant.destroy
+    redirect_to restaurants_path
+  end
+
+  private
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :phone, :address, :description)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :restaurants
 end
